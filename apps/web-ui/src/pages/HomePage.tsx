eb893eee4222759c6d0d56aa736f1682d7e213ed
Ben Moeller <benlynnmoeller@gmail.com>
2025-12-30 20:56:34 -0600

    Making image clickable and navigating to page

--- apps/web-ui/src/pages/HomePage.tsx
+++ apps/web-ui/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import useConfig from "../hooks/useConfig";
 import MediaInfoDrawer from "../components/MediaInfoDrawer";
 import type { MediaInfoDrawerType } from "../types/drawers.types";
 import useAnilistMediaQuery from "../hooks/useAnilistMediaQuery";
+import { useNavigate } from "react-router";
 
 function HomePage() {
     const [mediaInfoDrawer, setMediaInfoDrawer] = useState<MediaInfoDrawerType>({
@@ -13,6 +14,7 @@ function HomePage() {
     });
 
     const { isMobile } = useConfig();
+    const navigate = useNavigate();
     const mediaList = useAnilistMediaQuery();
 
     return (
@@ -35,7 +37,9 @@ function HomePage() {
                                 <ImageListItem key={media.id} className="element-slight-hover">
                                     <img
                                         src={media.coverImage.extraLarge}
-                                        style={{ borderRadius: '10px' }}
+                                        style={{ borderRadius: '10px', cursor: 'pointer' }}
+                                        alt={`${media.title.english} cover image`}
+                                        onClick={() => navigate(`/media/${media.id}`)}
                                     />
                                     <ImageListItemBar
                                         title={media.title.english}
